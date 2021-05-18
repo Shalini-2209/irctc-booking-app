569a4dcfa5bfde95e48b37647eb707fe24a655d7
Anbarsan <arsan.anbu97@gmail.com>
2021-05-18 13:50:24 +0530

    env

--- src/storage/Contracts.js
+++ src/storage/Contracts.js
@@ -5,11 +5,7 @@ const web = new web3("http://localhost:7545");
 
 const RemixContract = new web.eth.Contract(
   IrctcABI,
-<<<<<<< HEAD
-  "0x8f2B32Fac33A5709Bf6fa4F57A325Fe9De6fC855"
-=======
   `${process.env.REACT_APP_CONTRACT_KEY}`
->>>>>>> main
 );
 
 export default { web, RemixContract };
